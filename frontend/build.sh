c4d6f77a187c494720791a1bd75a00690845304e
UC-Travis <bartonti@mail.uc.edu>
2020-10-05 17:25:00 -0400

    testing

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
+npm install -g @vue/cli
 npm install -D # installs only dev dependencies
 npm install            # installs prod dependencies
 npm run build          # builds the Vue.js app
-#chicken
