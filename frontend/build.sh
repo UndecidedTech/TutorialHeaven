547127223e597f962fdd0dd998afeb28e20bee03
UC-Travis <bartonti@mail.uc.edu>
2020-10-05 19:10:04 -0400

    testing

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 npm install -g @vue/cli
-vue add cli-plugin-eslint
+vue add eslint
 npm install -D --no-shrinkwrap # installs only dev dependencies
 npm install            # installs prod dependencies
 npm run build          # builds the Vue.js app
