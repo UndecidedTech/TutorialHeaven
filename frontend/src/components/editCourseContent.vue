ee350f24f535120c499fbf2ee158ce620567af70
Alex <spartans4348@yahoo.com>
2020-10-15 15:39:24 -0400

    Video Content update

--- frontend/src/components/editCourseContent.vue
+++ frontend/src/components/editCourseContent.vue
@@ -18,10 +18,13 @@
       <textarea class="form-control" style="width: 100%" v-model="content.value" id="exampleFormControlTextarea1" rows="3" @change="updateSectionContent({sectionID: section._id, field: 'text', value: $event.target.value, contentID: content._id})"></textarea>
     </div>
     <div class="editor-item" v-else-if="content.type === 'image'">
-      <input type="text" v-model="content.value" @change="updateSectionContent({sectionID: section._id, field: 'text', value: $event.target.value, contentID: content._id})">
+      <input type="text" v-model="content.value" @change="updateSectionContent({sectionID: section._id, value: $event.target.value, contentID: content._id})">
       <img v-bind:src="content.value" alt="test" class="img-thumbnail w-25 h-25 mb-3">
     </div>
-    <div class="editor-item" v-else-if="content.type === 'video'"></div>
+    <div class="editor-item" v-else-if="content.type === 'video'">
+      <input type="text" v-model="content.value" @change="updateSectionContent({sectionID: section._id, value: $event.target.value, contentID: content._id})">
+      <iframe width="560" height="315" v-bind:src="content.value" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     <div class="editor-item" v-else-if="content.type === 'file'"></div>
   </div>
   </draggable>
