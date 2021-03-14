201f75fb42b77817180694c41df64464432fcf87
Bran <bpowers.soccer@gmail.com>
2021-03-13 23:47:05 -0500

    collecting subject performance for students

--- routes/statistics.js
+++ routes/statistics.js
@@ -49,6 +49,7 @@ router.get("/:courseID/", async (req, res) => {
     const Students = await User.find({ "courses._id": courseID }, "", { lean: true })
 
     // LEARNED SOMETHING NEW, THE _id HAS A "toString" method on it and a "equals" method
+    let subjects = {};
 
     await Students.forEach((student) => {
         student.courses.forEach((course) => {
@@ -56,6 +57,15 @@ router.get("/:courseID/", async (req, res) => {
                 course.results.forEach((result) => {
                     assessments.find((assessment) => {
                         if (assessment._id.equals(result._id)) {
+                            // loop through keys in result
+                            for (key in result.subjects) {
+                                // check key isn't already in subjects
+                                if (!(key in subjects)) {
+                                    subjects[key] = [0,0]
+                                }
+                                subjects[key][0] += results[key][0];
+                                subjects[key][1] += results[key][1];
+                            }
                             assessment.results.push(result.score)
                         }
                     })
@@ -71,15 +81,9 @@ router.get("/:courseID/", async (req, res) => {
         let sum = assessment.results.reduce((a, b) => a + b, 0)
         let avg = (sum / assessment.results.length) || 0
 
-        assessment["avgScore"] = avg
-        // calculate how many questions there are, 
+        assessment["avgScore"] = avg;
 
-        //weighted score with 50% of the weight being the STD DEV.
-
-        // 1-10 rating
-        // score 50% of deviation
-        // equation: (avg * .5) + ()
-        let stdDev = getStandardDeviation(assessment.results)
+        let stdDev = getStandardDeviation(assessment.results);
         console.log(stdDev);
         
         assessment["rating"] = ratingCalc(avg, stdDev)
